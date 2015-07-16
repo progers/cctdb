6491af0d5d644ee7eb0ec67538cfbc558c347bed
Philip Rogers <pdr@chromium.org>
2015-07-15 21:05:18 -0700

    Add two failing tests for impossible trees that should be detected

--- testCompare.py
+++ testCompare.py
@@ -53,5 +53,51 @@ class TestCompare(unittest.TestCase):
         # Not all calls in B are in A--newFn is not in A.
         self.assertEqual(compare._findStackDivergences(treeB, treeA, treeB), [['fn1()', 'fn2()', 'newFn()']])
 
+    #def testDuplicateCallCountDivergences(self):
+    #    treeA = self._simpleTestTree()
+    #    treeB = self._simpleTestTree()
+    #    secondFunctionAgain = {}
+    #    secondFunctionAgain["name"] = "fn2()"
+    #    treeB[0]["calls"].append(secondFunctionAgain)
+
+    #    # All calls in A are in B.
+    #    self.assertEqual(compare._findStackDivergences(treeA, treeB, treeA), [])
+
+    #    # Not all calls in B are in A--there are two fn1->fn2's in B but only one in A.
+    #    self.assertEqual(compare._findStackDivergences(treeB, treeA, treeB), [['fn1()', 'fn2()']])
+
+
+    #def testSubtreeConsistency(self):
+    #    # Tree where fn2 and fn3 are both called from fn1.
+    #    treeA = [];
+    #    aFirstCall = {}
+    #    aFirstCall["name"] = "fn1()"
+    #    aFirstCall["calls"] = []
+    #    treeA.append(aFirstCall)
+    #    aSecondCall = {}
+    #    aSecondCall["name"] = "fn2()"
+    #    aFirstCall["calls"].append(aSecondCall)
+    #    aThirdCall = {}
+    #    aThirdCall["name"] = "fn3()"
+    #    aFirstCall["calls"].append(aThirdCall)
+
+    #    # Tree where fn1 is called twice. The first call to fn1 calls fn2. The second call to fn1 calls fn3.
+    #    treeB = [];
+    #    bFirstCall = {}
+    #    bFirstCall["name"] = "fn1()"
+    #    treeB.append(bFirstCall)
+    #    bSecondCall = {}
+    #    bSecondCall["name"] = "fn2()"
+    #    bFirstCall["calls"] = [bSecondCall]
+    #    bThirdCall = {}
+    #    bThirdCall["name"] = "fn1()"
+    #    treeB.append(bThirdCall)
+    #    bFourthCall = {}
+    #    bFourthCall["name"] = "fn3()"
+    #    bThirdCall["calls"] = [bFourthCall]
+
+    #    self.assertNotEqual(compare._findStackDivergences(treeA, treeB, treeA), [])
+    #    self.assertNotEqual(compare._findStackDivergences(treeB, treeA, treeB), [])
+
 if __name__ == '__main__':
     unittest.main()
